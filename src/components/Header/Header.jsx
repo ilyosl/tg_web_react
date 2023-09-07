463717598cb17cc3de3c6782f7d51312dab1ce0f
Ilyos Lutfillayev <don@Ilyoss-MacBook-Pro.local>
2023-09-07 15:26:50 +0300

    adding styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../Button/button'
 import { useTelegram } from '../../hooks/useTelegram'
+import './Header.css';
 
 const Header = (props) => {
     const {onClose, user} = useTelegram();
